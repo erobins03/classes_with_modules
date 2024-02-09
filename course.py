0b93a663c35b9c7f916948cbd113fa54ba98b42e
erobins03 <rainbowroserobins@gmail.com>
2024-02-08 17:48:58 -0800

    updating

--- course.py
+++ course.py
@@ -2,4 +2,9 @@ class Course:
     def __init__(current_course, name, teacher):
         current_course.teacher = teacher
         current_course.name = name
+        current_course.student = []
         teacher.courses_taught.append(current_course)
+
+    def enroll_student(self, student):
+        self.student.append(student)
+        student.courses_enrolled.append(self)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from module import say_hello
 from person import Person
 from teacher import Teacher
 from course import Course
+from student import Student
 
 if __name__ == '__main__':
     # print(module.API_KEY)
@@ -13,7 +14,10 @@ if __name__ == '__main__':
     # print(a_person.name)
     hcde_teacher = Teacher("Adrian")
     hcde_class = Course("HCDE 310", hcde_teacher)
-    print(hcde_class.teacher.name)
-    for course in hcde_teacher.courses_taught:
-        print(course.name)
-    print(f"Teacher: {hcde_teacher.name} is teaching {hcde_teacher.courses_taught[0].name}")
+    hcde_student_in_systems = Student("Emma")
+    hcde_class.enroll_student(hcde_student_in_systems)
+
+    for student in hcde_class.student:
+        print(student.name)
+    # print(hcde_class.teacher.name)
+    # print(f"Teacher: {hcde_teacher.name} is teaching {hcde_teacher.courses_taught[0].name}")
